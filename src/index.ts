f7cde5cb018f77adfaaafc001c16c3ad26177f99
Bilguun <bbg.1015@gmail.com>
2020-07-24 17:20:15 -0500

    implemented abstract class for LinkedList,numbers and characters classes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([-345, 3, 3.5, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([45, 3, 3.5, -5, 0]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('sdfe');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
